2b864defac8054cf45a6e35219ad1c8fb21dfa2f
Egor Sezen <sezen.egor@mail.ru>
2025-06-13 14:44:29 +0300

    delete: project based on poetry, these files are useless

--- script_for_transer_dependencies_to_poetry.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-with open("requirements.txt") as f:
-    for line in f:
-        pkg = line.strip()
-        if pkg and not pkg.startswith("#"):
-            subprocess.run(["poetry", "add", pkg])
