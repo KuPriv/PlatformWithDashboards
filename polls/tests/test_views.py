fde06de2e1a5998ae4e500e7846ac1727d40938a
Egor Sezen <sezen.egor@mail.ru>
2025-06-14 00:19:26 +0300

    feat: add pytest-django, dependencies in pre-commit, add "run pytest" in django.yml, create pytest.ini for pytest and create unused model

--- polls/tests/test_views.py
+++ polls/tests/test_views.py
@@ -1,7 +1,8 @@
-from django.test import TestCase, Client
-from django.urls import reverse
-from django.contrib.auth.models import User
-from kombu.asynchronous.http import Response
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import NoReverseMatch, reverse
+
+from polls import views
 
 
 class ViewsTestCase(TestCase):
@@ -9,28 +10,29 @@ class ViewsTestCase(TestCase):
     def setUp(self):
         """Настройка данных для тестов"""
         self.client = Client()
-        self.user = User.objects.create_user(
-            username='testuser',
-            password='testpass123'
+        user_model = get_user_model()
+        self.user = user_model.objects.create_user(
+            username="testuser", password="testpass123"
         )
 
     def test_index_view(self):
         """Тест главной страницы polls"""
         try:
-            response = self.client.get(reverse('polls:index'))
+            response = self.client.get(reverse("polls:index"))
             self.assertEqual(response.status_code, 200)
-        except:
+        except NoReverseMatch:
             # Если URL не найден, создадим простой тест
-            response = self.client.get('/polls/')
+            response = self.client.get("/polls/")
             # Проверяем, что получаем какой-то ответ
             self.assertIn(response.status_code, [200, 301, 302, 404])
 
     def test_views_import(self):
         """Тест импорта views модуля"""
         try:
-            from polls import views
             # Проверяем, что модуль views существует
-            self.assertTrue(hasattr(views, '__name__'))
+            self.assertTrue(hasattr(views, "__name__"))
         except ImportError:
             # Если views не существует, тест все равно должен пройти
-            self.assertTrue(True, "Views module not found, but test passes")
+            self.skipTest("polls.views not found")
+        else:
+            self.assertTrue(hasattr(views, "__name__"))
