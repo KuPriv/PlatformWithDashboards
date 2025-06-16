af35845620d00b060a4c145b7bd8bf93382b08b8
Egor Sezen <sezen.egor@mail.ru>
2025-06-17 00:06:55 +0300

    chore: create test_db postgreSQL

--- PlatformWithDashboards/settings.py
+++ PlatformWithDashboards/settings.py
@@ -74,7 +74,7 @@ load_dotenv()
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "dashboard_db",
+        "NAME": os.getenv("NAME_DB"),
         "USER": "postgres",
         "PASSWORD": os.getenv("PASSWORD_DB"),
         "HOST": "localhost",
